a44cb0a3888e3614e33ee5cd5417da64d5f5276b
qqadi <qaisalqadi2000@gmail.com>
2024-06-11 14:52:46 +0300

    encpasulated decimal_to_binary in one method

--- conversions.py
+++ conversions.py
@@ -1,16 +1,13 @@
-binary_number = "11010101011"
-binary_list = [int(bit) for bit in binary_number]
-binary_list.reverse()
-
-def decimal_to_binary(binary_list):
+def decimal_to_binary(binary):
+    binary_list = [int(bit) for bit in str(binary)]
+    binary_list.reverse()
     decimal = 0
     place_value = 1
     for bit in range(len(binary_list)):
         place_value = place_value * 2
         if binary_list[bit] == 1:
             decimal = place_value + decimal
-    return decimal/2
+    print(f"Binary: {binary}")
+    print(f"Decimal: {int(decimal/2)}")
 
-y = decimal_to_binary(binary_list)
-print(f"Binary: {binary_number}")
-print(f"Decimal: {int(y)}")
+decimal_to_binary(1010101)
