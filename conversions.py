fcb95781c4762145faad2fcee54e0fdc507c9a8e
qqadi <qaisalqadi2000@gmail.com>
2024-06-12 13:40:01 +0300

    [TAS-4.5/4.6] method added

--- conversions.py
+++ conversions.py
@@ -51,4 +51,33 @@ def binary_to_octal(binary):
         i += 3
     return octal
 
+def hexadecimal_to_binary(hexadecimal):
+    hexadecimal_to_binary_map = {
+        '0': '0000', '1': '0001', '2': '0010', '3': '0011',
+        '4': '0100', '5': '0101', '6': '0110', '7': '0111',
+        '8': '1000', '9': '1001', 'A': '1010', 'B': '1011',
+        'C': '1100', 'D': '1101', 'E': '1110', 'F': '1111'
+    }
+    binary = ''
+    for digit in hexadecimal:
+        binary += hexadecimal_to_binary_map[digit]
+    return binary
+
+def binary_to_hexadecimal(binary):
+    binary_to_hexadecimal_map = {
+        '0000': '0', '0001': '1', '0010': '2', '0011': '3',
+        '0100': '4', '0101': '5', '0110': '6', '0111': '7',
+        '1000': '8', '1001': '9', '1010': 'A', '1011': 'B',
+        '1100': 'C', '1101': 'D', '1110': 'E', '1111': 'F'
+    }
+    while len(binary) % 4 != 0:
+        binary = '0' + binary
+    hexadecimal = ''
+    i = 0
+    while i < len(binary):
+        group = binary[i:i+4]
+        hexadecimal += binary_to_hexadecimal_map[group]
+        i += 4
+    return hexadecimal
+
 print(binary_to_octal('1101101'))
